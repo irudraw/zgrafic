3677bcda71ec18ef69898be04b639e1abde6c142
irudraw <41246008+irudraw@users.noreply.github.com>
2020-03-06 22:48:15 -0500

    Add files via upload

---
+++ dist/extensions/ext-locale/star/es.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'estrella',
+  buttons: [
+    {
+      title: 'Herramienta estrella'
+    }
+  ],
+  contextTools: [
+    {
+      title: 'Numero de lados',
+      label: 'puntos'
+    },
+    {
+      title: 'Puntas',
+      label: 'Puntas'
+    },
+    {
+      title: 'Tuerce la estrella',
+      label: 'Cambio radial'
+    }
+  ]
+};
