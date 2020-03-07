5d9aeec0e4140b644ff04b81c6725d08697a8ecc
irudraw <41246008+irudraw@users.noreply.github.com>
2020-03-06 21:39:58 -0500

    Add files via upload

---
+++ dist/extensions/ext-locale/connector/es.js
@@ -0,0 +1,11 @@
+export default {
+  name: 'Conector',
+  langList: [
+    {id: 'mode_connect', title: 'Conecta dos objetos'}
+  ],
+  buttons: [
+    {
+      title: 'Conecta dos objetos'
+    }
+  ]
+};
