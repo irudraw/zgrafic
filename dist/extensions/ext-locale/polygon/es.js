0bdc56c082f3f9e6930312e0b2d3351d466cdd08
irudraw <41246008+irudraw@users.noreply.github.com>
2020-03-06 22:31:01 -0500

    Add files via upload

---
+++ dist/extensions/ext-locale/polygon/es.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'polígono',
+  buttons: [
+    {
+      title: 'Herramienta de polígono'
+    }
+  ],
+  contextTools: [
+    {
+      title: 'Numero de lados',
+      label: 'lados'
+    }
+  ]
+};
