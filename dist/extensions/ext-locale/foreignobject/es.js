046ed70bb5c9b4c714f97dc17c715640a026d409
irudraw <41246008+irudraw@users.noreply.github.com>
2020-03-06 21:52:48 -0500

    Add files via upload

---
+++ dist/extensions/ext-locale/foreignobject/es.js
@@ -0,0 +1,25 @@
+export default {
+  name: 'objetoExtraño',
+  buttons: [
+    {
+      title: 'Herramienta de objetos extraños'
+    },
+    {
+      title: 'Editar contenido de objeto externo'
+    }
+  ],
+  contextTools: [
+    {
+      title: "Cambiar el ancho del objeto extraño",
+      label: 'w'
+    },
+    {
+      title: "Cambiar la altura del objeto extraño",
+      label: 'h'
+    },
+    {
+      title: "Cambiar el tamaño de fuente de foreignObject",
+      label: 'tamaño de fuente'
+    }
+  ]
+};
