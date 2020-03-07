9e6ab60caf4312091395dd39884a74c68c051eec
irudraw <41246008+irudraw@users.noreply.github.com>
2020-03-06 22:13:43 -0500

    Add files via upload

---
+++ dist/extensions/ext-locale/markers/es.js
@@ -0,0 +1,46 @@
+export default {
+  name: 'Markers',
+  langList: [
+    {id: 'nomarker', title: 'Sin marcador'},
+    {id: 'leftarrow', title: 'Flecha izquierda'},
+    {id: 'rightarrow', title: 'Flecha derecha'},
+    {id: 'textmarker', title: 'Marcador de texto'},
+    {id: 'forwardslash', title: 'Barra inclinada'},
+    {id: 'reverseslash', title: 'Barra invertida'},
+    {id: 'verticalslash', title: 'Barra vertical'},
+    {id: 'box', title: 'Cubo'},
+    {id: 'star', title: 'Estrella'},
+    {id: 'xmark', title: 'X'},
+    {id: 'triangle', title: 'Triangulo'},
+    {id: 'mcircle', title: 'Circulo'},
+    {id: 'leftarrow_o', title: 'Abrir flecha izquierda'},
+    {id: 'rightarrow_o', title: 'Abrir flecha derecha'},
+    {id: 'box_o', title: 'Abrir cubo'},
+    {id: 'star_o', title: 'Abrir estrella'},
+    {id: 'triangle_o', title: 'Abrir Triangulo'},
+    {id: 'mcircle_o', title: 'Abrir Circulo'}
+  ],
+  contextTools: [
+    {
+      title: 'Comience el marcador',
+      label: 's'
+    },
+    {
+      title: 'Seleccione el tipo de marcador de inicio'
+    },
+    {
+      title: 'Marcador medio',
+      label: 'm'
+    },
+    {
+      title: 'Seleccione el tipo de marcador medio'
+    },
+    {
+      title: 'Marcador final',
+      label: 'e'
+    },
+    {
+      title: 'Seleccionar tipo de marcador final'
+    }
+  ]
+};
